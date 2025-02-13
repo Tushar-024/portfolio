7fc858221501dfd3213f151fd7b00e999fb1d678
Tushar <tusharsingla024@gmail.com>
2025-02-13 13:40:07 +0530

    bug fixes

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -6,7 +6,9 @@ import pdf from "../../Assets/../Assets/Tushar_Resume_SDE.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+
+pdfjs.GlobalWorkerOptions.workerSrc = 'pdf.worker.min.js'
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
